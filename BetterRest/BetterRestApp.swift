2ca1c4f7c1fd268ff506f0f1c400e6e6f6f246fb
VijayaMadhavi <vijayamadhavi@hotmail.com>
2022-02-17 17:10:56 +0530

    first commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Vijaya Madhavi on 14/02/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
